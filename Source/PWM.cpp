017ff7b42d4342c19c45d801abdc012b07165c1b
Sebastian Badon <sebastianbadon@Sebastians-MacBook-Pro.local>
2018-09-05 15:13:36 -0400

    adding pwm slider

---
+++ Source/PWM.cpp
@@ -0,0 +1,51 @@
+/*
+  ==============================================================================
+
+    Pulse.cpp
+    Created: 3 Sep 2018 9:32:00pm
+    Author:  Sebastian Badon
+
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PWM.h"
+
+//==============================================================================
+PWM::PWM(SynthFrameworkAudioProcessor& p) :
+processor(p)
+{
+    pwmSlider.setSliderStyle(Slider::SliderStyle::LinearVertical);
+    pwmSlider.setRange(0, 0.99f);
+    pwmSlider.setValue(0);
+    pwmSlider.setTextBoxStyle(Slider::TextBoxBelow, true, 0, 0);
+    pwmSlider.addListener(this);
+    addAndMakeVisible(&pwmSlider);
+    addAndMakeVisible(&pwmLabel);
+    pwmLabel.attachToComponent(&pwmSlider, false);
+    pwmLabel.setText("PWM", dontSendNotification);
+    
+    pwmVal = new AudioProcessorValueTreeState::SliderAttachment (*processor.tree, "pwm", pwmSlider);
+
+}
+
+PWM::~PWM()
+{
+}
+
+void PWM::paint (Graphics& g)
+{
+   pwmSlider.setBounds(50, 30, 40, 100);
+}
+
+void PWM::resized()
+{
+    // This method is where you should set the bounds of any child
+    // components that your component contains..
+
+}
+
+void PWM::sliderValueChanged(Slider* slider)
+{
+    
+}
---
+++ Source/PWM.h
@@ -0,0 +1,35 @@
+/*
+  ==============================================================================
+
+    Pulse.h
+    Created: 3 Sep 2018 9:32:00pm
+    Author:  Sebastian Badon
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+//==============================================================================
+/*
+*/
+class PWM    : public Component, public Slider::Listener
+{
+public:
+    PWM(SynthFrameworkAudioProcessor& p);
+    ~PWM();
+
+    void paint (Graphics&) override;
+    void resized() override;
+    void sliderValueChanged (Slider*) override;
+
+private:
+    SynthFrameworkAudioProcessor& processor;
+    Slider pwmSlider;
+    Label pwmLabel;
+    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> pwmVal;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PWM)
+};
